0cf697218c6b5f6cbc3fffc24308b2a8ff165f79
Darren Cook <thelurkingdev@gmail.com>
2019-11-06 22:18:02 -0500

    Wired up Ok and Cancel buttons.

--- Assets/Input Window/UiInputWindow.cs
+++ Assets/Input Window/UiInputWindow.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UiInputWindow : MonoBehaviour
 {
+    private Button _okButton;
+    private Button _cancelButton;
+
     private void Awake()
     {
+        GetButtonReferences();
+        SetButtonClickHandlers();
         Hide();
     }
 
@@ -15,5 +21,38 @@ public class UiInputWindow : MonoBehaviour
     public void Hide()
     {
         this.gameObject.SetActive(false);
+    }
+
+    private void GetButtonReferences()
+    {
+        var buttons = GetComponentsInChildren<Button>();
+        foreach (Button button in buttons)
+        {
+            if (button.name.Contains("Cancel"))
+            {
+                _cancelButton = button;
+            }
+
+            if (button.name.Contains("Ok"))
+            {
+                _okButton = button;
+            }
+        }
+    }
+
+    private void SetButtonClickHandlers()
+    {
+        _cancelButton.onClick.AddListener(HandleCancelButtonClick);
+        _okButton.onClick.AddListener(HandleOkButtonClick);
+    }
+
+    private void HandleCancelButtonClick()
+    {
+        Debug.Log("Cancel clicked!");
+    }
+
+    private void HandleOkButtonClick()
+    {
+        Debug.Log("Ok clicked!");
     }
 }
